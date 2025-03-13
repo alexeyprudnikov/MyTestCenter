adac5b1ef5d26d40f5993f70058da16caa055d8a
Alexey Prudnikov <a.prudnikov@neusta.de>
2025-03-13 11:48:56 +0100

    add davinci test service

--- src/Controller/WebApp/TestController.php
+++ src/Controller/WebApp/TestController.php
@@ -5,13 +5,14 @@ namespace App\Controller\WebApp;
 use App\Dto\Schema\GoogleGenAi\HotelsResponse;
 use App\Dto\Schema\GoogleGenAi\RecipesResponse;
 use App\Service\Ai\GoogleGenAiService;
+use App\Service\DaVinciService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(name: 'test_gemini_')]
-class TestGoogleGenAiController extends AbstractController
+#[Route(name: 'test_')]
+class TestController extends AbstractController
 {
     #[Route('/test_gemini', name: 'index')]
     public function index(): Response
@@ -53,5 +54,13 @@ class TestGoogleGenAiController extends AbstractController
             ]
         );
         return $this->json(['content' => $content]);
+    }
+
+    #[Route('/test_davinci', name: 'davinci')]
+    public function daVinci(
+        DaVinciService $daVinciService,
+    ): JsonResponse
+    {
+        return $this->json($daVinciService->proceed());
     }
 }
---
+++ src/Service/DaVinciService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+use Symfony\Component\HttpClient\HttpOptions;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DaVinciService
+{
+    private HttpClientInterface $availabilityClient;
+    private array $body;
+
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly SerializerInterface $serializer,
+    )
+    {
+        $this->availabilityClient = $this->httpClient->withOptions(
+            (new HttpOptions())
+                ->setBaseUri('https://var.ssc-test.davincicloud.de/restbaseaddress/')
+                ->setHeaders([
+                    'Content-Type' =>'application/json',
+                    'Accept' => 'application/json'
+                ])
+            ->toArray()
+        );
+        $this->body = [
+            "Authentication" => [
+                "User" => "IN",
+                "Password" => "Web"
+            ]
+        ];
+    }
+
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function proceed(): array
+    {
+        try {
+            $request = $this->availabilityClient->request(
+                'POST',
+                'GetSingleServicesAndAvailabilities',
+                [
+                    'body' => $this->serializer->encode($this->getAvailabilityBody(), 'json')
+                ]
+            );
+            if ($request->getStatusCode() !== 200) {
+                throw new BadRequestHttpException();
+            }
+            return $this->serializer->decode($request->getContent(), 'json');
+        } catch (Exception $exception) {
+            throw new $exception;
+        }
+    }
+
+    private function getAvailabilityBody(): array
+    {
+        return array_merge(
+            $this->body, [
+                "SearchCriteria" => [
+                    "SearchText" => "NMANK",
+                    "Agency" => 1,
+                    "From" => "/Date(1749754499000)/",
+                    "To" => "/Date(1750013699000)/",
+                    "Durations" => [
+                        5
+                    ],
+                    "Adults" => 2,
+                    "CatalogCode" => "AP25",
+                    "ServiceTypeToSearch" => 0
+                ],
+                "BookingDateTime" => "/Date(1671667200000)/",
+                "BookingSource" => 0,
+                "ShowNotAvailable" => true,
+                "PurchasePrices" => false,
+                "IsInteractiveRequest" => true,
+                "DoNotCalculatePrices" => false,
+                "DoNotReturnServiceTypes" => false
+            ]
+        );
+    }
+}
